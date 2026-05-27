3a25046fff3aa8d6ede28d0c88b0a0fd7b70f4eb
SAnisthuy <sansithu215@gmail.com>
2026-05-28 08:11:49 +1200

    Update throwable.gd

--- Scripts/throwable.gd
+++ Scripts/throwable.gd
@@ -2,17 +2,25 @@ extends Area2D
 
 var speed = 200
 var dir: float
+var velocity: Vector2
+
+func _ready():
+	velocity = Vector2.RIGHT.rotated(dir) * speed
 
 func _physics_process(delta):
-	position += Vector2.RIGHT.rotated(dir) * speed * delta
-	position.y += 20 * delta
+	velocity.y += 1
 	
+	position += velocity * delta
+	
+	rotation = velocity.angle()
 
 
 func _on_body_entered(body: Node2D) -> void:
 	for thing in get_overlapping_bodies():
 		if thing.has_method("enemy"):
 			thing.take_damage(30)
+
 	if body.has_method("player"):
 		return
+		
 	queue_free()
